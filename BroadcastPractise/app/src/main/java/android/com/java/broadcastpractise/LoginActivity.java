b71b7f2588b7949bfd5f577cd145067af886aef9
Sample <913925047@qq.com>
2017-02-23 23:13:15 +0800

    由于第一行代码的问题，权限获取不对，重新查找发现需要打开权限，重新添加权限代码

--- BroadcastPractise/app/src/main/java/android/com/java/broadcastpractise/LoginActivity.java
+++ BroadcastPractise/app/src/main/java/android/com/java/broadcastpractise/LoginActivity.java
@@ -1,7 +1,10 @@
 package android.com.java.broadcastpractise;
 
 import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -20,6 +23,13 @@ public class LoginActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
+        if (Build.VERSION.SDK_INT >= 23) {
+            if (! Settings.canDrawOverlays(LoginActivity.this)) {
+                Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+                        Uri.parse("package:" + getPackageName()));
+                startActivityForResult(intent,10);
+            }
+        }
         accountEdit = (EditText)findViewById(R.id.account);
         passwordEdit = (EditText)findViewById(R.id.password);
         login = (Button)findViewById(R.id.login);
@@ -37,5 +47,17 @@ public class LoginActivity extends BaseActivity {
                 }
             }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == 10) {
+            if (Build.VERSION.SDK_INT >= 23) {
+                if (!Settings.canDrawOverlays(this)) {
+                    // SYSTEM_ALERT_WINDOW permission not granted...
+                    Toast.makeText(LoginActivity.this,"not granted",Toast.LENGTH_SHORT);
+                }
+            }
+        }
     }
 }
