42188dab00b8a4a65273ffe9cbd07012a961f0d2
Sample <913925047@qq.com>
2017-02-26 22:00:17 +0800

    遗漏的一些文件上传，也不知道有什么用，加上一个contentresolver的小demo

--- ContentProviderDemo/app/src/main/java/android/com/java/contentproviderdemo/MainActivity.java
+++ ContentProviderDemo/app/src/main/java/android/com/java/contentproviderdemo/MainActivity.java
@@ -1,13 +1,50 @@
 package android.com.java.contentproviderdemo;
 
-import android.support.v7.app.AppCompatActivity;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    ListView contactsView;
+    ArrayAdapter<String> adapter;
+    List<String> contactsList = new ArrayList<String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        contactsView = (ListView)findViewById(R.id.contacts_vies);
+        adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,contactsList);
+        contactsView.setAdapter(adapter);
+
+    }
+    public void readContacts(){
+        Cursor cursor = null;
+        try{
+            cursor = getContentResolver().query(
+                    ContactsContract.CommonDataKinds.Phone.CONTENT_URI,null,null,null,null
+            );
+            while(cursor.moveToNext()){
+                String displayName = cursor.getString(cursor.getColumnIndex(
+                        ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME
+                ));
+                String number = cursor.getString(cursor.getColumnIndex(
+                        ContactsContract.CommonDataKinds.Phone.NUMBER
+                ));
+                contactsList.add(displayName+"\n"+number);
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally {
+            if (cursor!=null){
+                cursor.close();
+            }
+        }
     }
 }
