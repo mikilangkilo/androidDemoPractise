5290065f5acb0cdb96d8cd413fa5c64059e5f3f9
Sample <913925047@qq.com>
2017-07-19 22:42:24 +0800

    searchviewlistview demo

---
+++ SearchViewListView/app/src/main/java/android/com/java/searchviewlistview/MainActivity.java
@@ -0,0 +1,13 @@
+package android.com.java.searchviewlistview;
+
+import android.app.ListActivity;
+import android.os.Bundle;
+
+public class MainActivity extends ListActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
