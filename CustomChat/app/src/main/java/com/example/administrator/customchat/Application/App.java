69dbf685f4e14a07a0ce8e782f819adab73a9c1b
Sample <913925047@qq.com>
2017-10-10 23:25:17 +0800

    customChatDemo

---
+++ CustomChat/app/src/main/java/com/example/administrator/customchat/Application/App.java
@@ -0,0 +1,17 @@
+package com.example.administrator.customchat.Application;
+
+import android.app.Application;
+
+import com.squareup.leakcanary.LeakCanary;
+
+/**
+ * Created by Administrator on 2017/10/10.
+ */
+
+public class App extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        LeakCanary.install(this);
+    }
+}
